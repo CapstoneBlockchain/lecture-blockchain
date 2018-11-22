dac854c0d18c148549a04399c52c2e91c6661f65
minsoo <als950901@gmail.com>
2018-11-23 04:24:11 +0900

    error fixed

--- userApplication/TokenWeb3.js
+++ userApplication/TokenWeb3.js
@@ -210,6 +210,7 @@ $(document).ready(function(){
 						if (Number(document.getElementById("today_text").value) > result){
 							alert("You don't have enough token");
 						} else {
+							alert("Success ".itemName." check.");
 							document.getElementById(itemName).disabled = false;
 						}
 					}
